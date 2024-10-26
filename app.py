854b10d6c1bf02d0261c05f6930141492a5361aa
Guilherm11-bit <guilhermesanma10927@gmail.com>
2024-10-26 11:03:48 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+numero1 = int(input("Digite o primeiro numero"))
+numero2 = int(input("Digite o segundo numero"))
+
+soma = numero1 + numero2
+subtração = numero1 - numero2
+divisao = numero1 / numero2
+multiplicacao = numero1 * numero2 
+
+print("soma: ",soma)
+print("subtração: ", subtração)
+print("Divisão: ", divisao)
+print("Multiplicação: ", multiplicacao)
